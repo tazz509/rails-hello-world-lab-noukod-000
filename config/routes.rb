3f7de16007e7a42ab3100aac5e76066bd316d0cd
carly <carly@noukod.com>
2019-01-21 15:42:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,8 @@ Rails.application.routes.draw do
   #     end
   #   end
 
+  get 'hello', to: 'static#hello'
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
